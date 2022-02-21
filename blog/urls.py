f32a8a6348152807fc6a1896bfff8a5c4e551bc7
hpsarvari <hossein.psarvari@gmail.com>
2022-02-21 21:10:37 +0100

    036

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import ArticleList, ArticleDetail, CategoryList
+
+app_name = 'blog'
+urlpatterns = [
+    path('', ArticleList.as_view(), name='home'),
+    path('page/<int:page>', ArticleList.as_view(), name='home'),
+    path('article/<slug:slug>/', ArticleDetail.as_view(), name='detail'),
+    path('category/<slug:slug>/', CategoryList.as_view(), name='category'),
+    path('category/<slug:slug>/<int:page>/', CategoryList.as_view(), name='category'),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+#from django.http import HttpResponse
+from .models import Article, Category
+
+
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 3
+
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug=slug)
+
+
+class CategoryList(ListView):
+    paginate_by = 3
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.active(), slug=slug)
+        return category.articles.published()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
+
