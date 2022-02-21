6e291dfb9adc5bb6ecea1094048bdd83353c37dc
hpsarvari <hossein.psarvari@gmail.com>
2022-02-21 22:24:05 +0100

    039learn

--- blog/models.py
+++ blog/models.py
@@ -60,9 +60,6 @@ class Article(models.Model):
         return jalali_convertor(self.publish)
     jpublish.short_description = 'زمان انتشار'
 
-    def category_published(self):
-        return self.category.filter(status=True)
-
     def thumbnail_tag(self):
         return format_html("<img width=100 height=75 style='border-radius: 5px;' src='{}'>".format(self.thumbnail.url))
 
